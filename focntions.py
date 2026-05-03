830b0abe9ed771d553ce1502ea06c8c48223b7da
lamine <lamine.idoughi@gmail.com>
2026-05-03 23:58:55 +0200

    lecture carte

--- focntions.py
+++ focntions.py
@@ -12,7 +12,7 @@ COLORS = {
 }
 
 class Bus:
-    def __init__(self, taille: int, direction: str, couleur: int, x: int, y: int, visite: bool, charge: int):
+    def __init__(self, taille: int, direction: str, couleur: int, x: int, y: int, visite: bool, charge: int, capacite: int):
         self.taille = taille
         self.direction = direction  # U, D, L, R
         self.couleur = couleur
@@ -20,9 +20,10 @@ class Bus:
         self.y = y
         self.visite = visite
         self.charge=charge
+        self.capacite=capacite
 
     def __repr__(self):
-        return f"Bus(taille={self.taille}, direction='{self.direction}', couleur={self.couleur}, x={self.x}, y={self.y}, visite={self.visite}, charge={self.charge})"
+        return f"Bus(taille={self.taille}, direction='{self.direction}', couleur={self.couleur}, x={self.x}, y={self.y}, visite={self.visite},capacite={self.capacite}, charge={self.charge})"
 
 
 
@@ -43,9 +44,9 @@ def lire_carte(carte):
         ligne=[]
         for c in range(0,len(b),4):     #Boucle pour construire les lignes de la grille
             if(b[c] != "X"):
-                bus=Bus(x = i ,y=c//4 , taille=int(b[c]),  direction=b[c + 1], couleur=int(b[c + 2]),visite=False,charge=0)
+                bus=Bus(x = i ,y=c//4 , capacite=int(b[c]), taille=int(b[c])//2, direction=b[c + 1], couleur=int(b[c + 2]),visite=False,charge=0)
             else:
-                bus=Bus(x = i ,y=c//4 , taille=0,  direction="X", couleur="X",visite=False,charge=0)
+                bus=Bus(x = i ,y=c//4 , capacite=0,taille=0,  direction="X", couleur="X",visite=False,charge=0)
             ligne.append(bus)
         grille.append(ligne)
 
@@ -55,7 +56,7 @@ def lire_carte(carte):
         for j, bus in enumerate(ligne):
             if not bus.visite:
 
-                if bus.taille==2:
+                if bus.capacite==2:
                     bus.visite=True
                     chauffeur.append(bus)
 
@@ -65,22 +66,23 @@ def lire_carte(carte):
 
                         chauffeur.append(bus)
 
-                        if bus.taille!=0:
-                            for k in range(i,i+bus.taille//2):
+                        if bus.capacite!=0:
+                            for k in range(i,i+bus.taille):
                                 grille[k][j].visite=True
                         else:
                             k=i
                             while bus.couleur == grille[k][j].couleur and grille[k][j].direction == "U" and k<len(grille):
                                 grille[k][j].visite = True
                                 k+=1
+                            bus.taille=k-i
 
 
                     elif bus.direction == "L":
 
                         chauffeur.append(bus)
 
-                        if bus.taille != 0:
-                            for k in range(j, j + bus.taille // 2):
+                        if bus.capacite != 0:
+                            for k in range(j, j + bus.taille ):
                                 grille[i][k].visite = True
 
                         else:
@@ -88,44 +90,72 @@ def lire_carte(carte):
                             while k < len(ligne) and grille[i][k].couleur == bus.couleur and grille[i][k].direction == "L":
                                 grille[i][k].visite = True
                                 k += 1
+                            bus.taille = k - j
 
                     elif bus.direction=="D":
 
-                        if bus.taille != 0:
-                            for k in range(i, i+bus.taille // 2 ):
+                        if bus.capacite != 0:
+                            for k in range(i, i+bus.taille  ):
                                 grille[k][j].visite = True
                         else:
                             k = i
                             while bus.couleur == grille[k][j].couleur and grille[k][j].direction == "D" and k < len(grille):
                                 grille[k][j].visite = True
                                 k += 1
-                            k-=1
-                        chauffeur.append(grille[k][j])
+
+                            grille[k-1][j].taille=k-i
+                        chauffeur.append(grille[k-1][j])
 
                     elif bus.direction == "R":
 
-                        if bus.taille != 0:
-                            for k in range(j, j+bus.taille // 2 ):
+                        if bus.capacite != 0:
+                            for k in range(j, j+bus.taille):
                                 grille[i][k].visite = True
                         else:
                             k = j
                             while bus.couleur == grille[i][k].couleur and grille[i][k].direction == "R" and k < len(ligne):
                                 grille[i][k].visite = True
                                 k += 1
-                            k-=1
-                        chauffeur.append(grille[i][k])
+
+                            grille[k - 1][j].taille =k - j
+                        chauffeur.append(grille[i][k-1])
+
     return chauffeur, personnages, taille_parking
 
+def est_jouable(grille, bus):
 
+        if bus.direction=="U":
+            if bus.x == 0:
+                return True
+            else:
+                for i in range(bus.x-1,-1,-1):
+                    if grille[i][bus.y].direction != "X":
+                        return False
 
+        if bus.direction=="L":
+            if bus.y == 0:
+                return True
+            else:
+                for i in range(bus.y-1,-1,-1):
+                    if grille[bus.x][i].direction != "X":
+                        return False
 
+        if bus.direction=="D":
+            if bus.x == len(grille)-1:
+                return True
+            else:
+                for i in range(bus.x+1,len(grille)):
+                    if grille[i][bus.y].direction != "X":
+                        return False
 
-
-
-
-
-
-
+        if bus.direction=="R":
+            if bus.y == len(grille[c.x])-1:
+                return True
+            else:
+                for i in range(bus.y+1,len(grille[c.x])):
+                    if grille[bus.x][i].direction != "X":
+                        return False
+        return True
 
 
 
